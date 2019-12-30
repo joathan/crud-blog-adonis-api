201f3092fd3c71d79d2da0223c0391049574ce3d
Joathan Francisco <joathanf@gmail.com>
2019-12-30 02:49:04 -0300

    model Articles implementado

---
+++ database/migrations/1577684436642_article_schema.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ArticleSchema extends Schema {
+  up () {
+    this.create('articles', (table) => {
+      table.increments()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.string('title').notNullable()
+      table.text('body').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('articles')
+  }
+}
+
+module.exports = ArticleSchema
